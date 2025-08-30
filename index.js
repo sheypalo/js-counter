a33b10a4450fa6b9441b22af424bdb3fc357b83f
albedo <nicoleshaine.t.palo@gmail.com>
2025-08-30 13:29:40 +0800

    added reset button

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-let saveEL = document.getElementById("save-el")
-let countEL =  document.getElementById("count-el")
-let count = 0
-
+let saveEL = document.getElementById("save-el");
+let countEL = document.getElementById("count-el");
+let resetEL = document.getElementById("reset-btn");
+let count = 0;
 
 function increment() {
-    count += 1
-    countEL.innerText = count
-    
+    count += 1;
+    countEL.innerText = count;
 }
 
 function save() {
-    console.log(count)
-    saveCount = count + "-"
-    saveEL.innerText += saveCount
+    console.log(count);
+    let saveCount = count + "-";
+    saveEL.innerText += saveCount;
 }
 
-
+function reset() {
+    count = 0; 
+    countEL.innerText = count; 
+    
+}
